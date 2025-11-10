da7775a54001a13a720fa46b583fec8d69bea326
User Charkes <charkes.hanna@gmail.com>
2025-11-10 11:23:37 +0200

    updated eatMeatReturnsPredatorsFood in FelineSpyTest

--- src/test/java/com/example/FelineSpyTest.java
+++ src/test/java/com/example/FelineSpyTest.java
@@ -6,18 +6,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.Mockito;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class FelineSpyTest {
+
 
     @Test
     void eatMeatReturnsPredatorsFood () throws Exception {
         Feline feline = new Feline();
         Feline felineSpy = Mockito.spy(feline);
-        Mockito.when(felineSpy.getFood("Хищник")).thenReturn(List.of("Котлеты", "Отбивные"));
 
+        List <String> expected = List.of("Котлеты", "Отбивные");
+        Mockito.when(felineSpy.getFood("Хищник")).thenReturn(expected);
         List<String> actual = felineSpy.eatMeat();
 
-        assertEquals(List.of("Котлеты", "Отбивные"), actual);
+        assertEquals(expected, actual);
+        Mockito.verify(felineSpy).getFood("Хищник");
     }
 }
